dad716369680a72d46b2ef4ccb256df1770b38c0
PXgamer <owzie123@gmail.com>
2017-09-05 10:59:30 +0100

    Updated the extension code to make it smoother, and support right-click on files

--- extension.js
+++ extension.js
@@ -1,13 +1,18 @@
 var vscode = require('vscode');
+var path = require('path');
 var exec = require('child_process').exec;
-var config = vscode.workspace.getConfiguration('hg-workbench-open');
+var config = vscode.workspace.getConfiguration('hgwo');
 
 function activate(context) {
-    var disposable = vscode.commands.registerCommand('extension.TortoiseHg', function (contextInfo) {
+    var disposable = vscode.commands.registerCommand('hgwo.TortoiseHg', function (contextInfo) {
         var pathName = contextInfo.fsPath;
 
+        if (contextInfo.scheme === 'file') {
+            pathName = path.dirname(pathName);
+        }
+
         exec("thgw.exe -R \"" + pathName + "\"");
-        
+
         if (config.notify) {
             vscode.window.showInformationMessage('Opening ' + pathName + ' in TortoiseHg');
         }

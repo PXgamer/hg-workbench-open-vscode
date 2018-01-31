142d1a23a4bed0e2b4a119c6208957100a08dd1d
pxgamer <owzie123@gmail.com>
2018-01-31 15:18:17 +0000

    Update to use ES6/7

--- extension.js
+++ extension.js
@@ -1,27 +1,27 @@
-var vscode = require('vscode');
-var path = require('path');
-var exec = require('child_process').exec;
-var config = vscode.workspace.getConfiguration('hgwo');
+import vscode from 'vscode';
+import path from 'path';
+import {exec} from 'child_process';
+const config = vscode.workspace.getConfiguration('hgwo');
 
 function activate(context) {
-    var disposable = vscode.commands.registerCommand('hgwo.TortoiseHg', function (contextInfo) {
-        var pathName = contextInfo.fsPath;
+  const disposable = vscode.commands.registerCommand('hgwo.TortoiseHg', contextInfo => {
+    let pathName = contextInfo.fsPath;
 
-        if (contextInfo.scheme === 'file') {
-            pathName = path.dirname(pathName);
-        }
+    if (contextInfo.scheme === 'file') {
+      pathName = path.dirname(pathName);
+    }
 
-        exec("thgw.exe -R \"" + pathName + "\"");
+    exec(`thgw.exe -R "${pathName}"`);
 
-        if (config.notify) {
-            vscode.window.showInformationMessage('Opening ' + pathName + ' in TortoiseHg');
-        }
-    });
+    if (config.notify) {
+      vscode.window.showInformationMessage(`Opening ${pathName} in TortoiseHg`);
+    }
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
-exports.activate = activate;
+export {activate};
 
 function deactivate() {
 }
-exports.deactivate = deactivate;
+export {deactivate};
